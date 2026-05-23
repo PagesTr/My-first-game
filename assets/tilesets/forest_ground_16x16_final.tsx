04e9508a02e1117e63a875147b9ed503a5771424
PagesTr <tristan.pages@hotmail.com>
2026-05-23 17:28:14 +0200

    oui

---
+++ assets/tilesets/forest_ground_16x16_final.tsx
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="forest_ground_16x16_final" tilewidth="16" tileheight="16" tilecount="60" columns="12">
+ <image source="C:/Users/Trist/Downloads/forest_ground_16x16_final.png" width="192" height="80"/>
+ <wangsets>
+  <wangset name="Jeu de Tuiles Sans Nom" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="2" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="12" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="13" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="14" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="15" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="24" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="25" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="26" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="27" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="36" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="37" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="38" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="39" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="50" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="51" wangid="1,0,0,0,1,0,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
