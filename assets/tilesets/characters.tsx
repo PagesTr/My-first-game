9c2bd6fb6d94faabdb4050094117d43918688dc0
PagesTr <tristan.pages@hotmail.com>
2026-05-24 00:01:52 +0200

    oui

---
+++ assets/tilesets/characters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="characters" tilewidth="32" tileheight="48" tilecount="636" columns="40">
+ <image source="../external/FANTASY RPG - Medieval townsfolk (by Franuka) v1.0/characters.png" width="1280" height="384"/>
+</tileset>
---
+++ assets/tilesets/characters_16x24.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="characters_16x24" tilewidth="16" tileheight="24" tilecount="320" columns="40">
+ <image source="../external/FANTASY RPG - Medieval townsfolk (by Franuka) v1.0/characters_16x24.png" width="640" height="192"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
