6850b95f8e588f3f635fed90903f36afdd215f59
PagesTr <tristan.pages@hotmail.com>
2026-05-31 22:06:46 +0200

    oui

--- assets/tilesets/pixe_crawlers_doors.tsx
+++ assets/tilesets/pixe_crawlers_doors.tsx
@@ -1,4 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="pixe_crawlers_doors" tilewidth="16" tileheight="16" tilecount="625" columns="25">
  <image source="../external/pixel_crawler_free_pack/spritesheets/environment/structures/buildings/props.png" width="400" height="400"/>
+ <wangsets>
+  <wangset name="Barrière bois" type="mixed" tile="-1">
+   <wangcolor name="barrière" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="276" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="277" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="278" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="300" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="301" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="303" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="304" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="325" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="329" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="350" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="351" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="353" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="354" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="376" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="377" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="378" wangid="1,0,0,0,1,1,1,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
